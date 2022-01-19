db4457f184d30ca2b00270a206f7a1514bc9a067
unknown <evrasalexander@gmail.com>
2022-01-18 22:22:55 -0500

    homework

---
+++ app.js
@@ -0,0 +1,15 @@
+function addition(numberOne, numberTwo){
+    console.log(numberOne + numberTwo)
+};
+
+function subtract(numberOne, numberTwo){
+    console.log(numberOne - numberTwo)
+};
+
+function multiplication(numberOne, numberTwo){
+    console.log(numberOne * numberTwo)
+};
+
+function division(numberOne, numberTwo){
+    console.log(numberOne / numberTwo)
+};
